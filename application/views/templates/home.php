b55e93dfa597d10dbf43b2a91ac8b2c9b0c2b4ba
Gamertod <gamertod@gamertod.tk>
2019-06-16 19:30:07 -0400

    navigation menu and icons added

--- application/views/templates/home.php
+++ application/views/templates/home.php
@@ -3,10 +3,34 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?php echo APPNAME; ?></title>
+	<link rel="stylesheet" href="<?php echo base_url() ?>stylesheets/css/all.css">
 	<link rel="stylesheet" href="<?php echo base_url() ?>stylesheets/css/home_styles.css">
 </head>
 <body>
 	<header>
+		<nav>
+			<div class="nav-content">			
+				<div class="logo">
+					<a href="#">Barbacoas</a>
+				</div>
+
+				<ul class="links">
+					<li><a href="#">Link #1</a></li>
+					<li><a href="#">Link #2</a></li>
+					<li><a href="#">Link #3</a></li>
+				</ul>
+
+				<ul class="users">
+					<li title="Iniciar Session"><a href="#">
+						<i class="fas fa-sign-in-alt"></i>
+					</a></li>
+					<li title="Registrarse"><a href="#">
+						<i class="fas fa-user-plus"></i>
+					</a></li>
+				</ul>
+			</div>
+		</nav>
+		<div class="header-overlay"></div>
 		<div class="header-content">
 			<h2>Lorem ipsum dolor sit.</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores repellendus eos impedit modi harum ratione ullam et tenetur dolore doloribus ex, sit culpa repudiandae nemo unde debitis obcaecati labore, quo!</p>
