852c834ae55f787358a80d45f8bb7dd27937d630
Gamertod <gamertod@gamertod.tk>
2019-07-14 15:56:21 -0400

    Product summary structure done

--- application/views/inc/product_modal.php
+++ application/views/inc/product_modal.php
@@ -1,5 +1,30 @@
 <div class="product_modal_wrapper">
 	<div class="product_wrap">
-		<h2>Lorem ipsum dolor.</h2>
+		<div class="left"></div>
+		<div class="right">
+			<h2>Lorem ipsum dolor.</h2>
+			<div class="description">
+				<h3>Lorem ipsum dolor.</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat quibusdam, hic cumque deserunt porro expedita voluptatibus, sed similique sit praesentium.</p>
+			</div>
+
+			<div class="amount">
+				<p>Cantidad:</p>
+
+				<span class="amount_wrapper">
+					<span class="more">
+						<i class="fas fa-plus"></i>
+					</span>
+
+					<span class="counter">1</span>
+
+					<span class="less">
+						<i class="fas fa-minus"></i>
+					</span>
+				</span>
+			</div>
+
+			<button class="cesta_btn">AÑADIR A LA CESTA</button>
+		</div>
 	</div>
 </div>
