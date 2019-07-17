c3d48d1ddb949128b465165234287b6d7eb47e2d
Gamertod <gamertod@gamertod.tk>
2019-07-17 00:44:18 -0400

    Building mis barbacoas view

---
+++ application/views/inc/item.php
@@ -0,0 +1,10 @@
+<div class="item">
+	<span id="item_view" class="view view_item_btn"><i class="far fa-eye"></i></span>
+
+	<div class="image-wrap">
+		<img src="<?php echo base_url() ?>img/item-test.png" alt="">	
+	</div>
+	<div class="item-content">
+		<a href="#">Lorem ipsum dolor sit.</a>
+	</div>
+</div>
